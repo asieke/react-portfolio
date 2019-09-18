4504a7f2d6d58771372aa7ee9a58bd06c67fdd7c
Alex Sieke <asieke@gmail.com>
2019-09-18 15:40:25 -0700

    added custom react components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Alex, a rare Siekechu</p>
+                <p>gotta catch em all!</p>
+            </div>
+        )
+    }
+}
+
+
+export default App;
