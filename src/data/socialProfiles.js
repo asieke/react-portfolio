04afa9be296828bf56f4733f899df2be09607906
Alex Sieke <asieke@gmail.com>
2019-09-19 14:55:48 -0700

    finished up social media profiles etc

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:asieke@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/asieke',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/asieke/',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'https://twitter.com/asieke',
+    image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
